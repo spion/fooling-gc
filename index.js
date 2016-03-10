069c0631388ae41de89a6598947f9aa70950690a
Gorgi Kosev <gorgi.kosev@gmail.com>
2016-03-10 14:20:48 +0000

    run gc if exposed

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 var argv = require('yargs').argv
 var through = require('through2')
 var split = require('split2')
+var process = require('process')
 
-var maxStorageSize = argv['size'] || 10000
-var interval = argv['log'] || 1;
-
-
+var maxStorageSize = argv['storageSize'] || 10000
+var repetitions    = argv['repetitions'] || 30
 
 function source() {
   var s = through()
   function generate() {
-    var n = Math.random()
-    var data = {number: n, field: n, test: n}
-    s.push(JSON.stringify(data) + '\n')
-    setImmediate(generate, 0)
-    //process.nextTick(generate)
+    for (var k = 0; k < maxStorageSize / 1000; ++k) {
+      var n = Math.random()
+      var data = {number: n, field: n, test: n}
+      s.push(JSON.stringify(data) + '\n')
+    }
+    setTimeout(generate, 1)
   }
-  process.nextTick(generate)
+  generate()
   return s;
 }
 
 
 function processor() {
   var storage = []
-  var gens    = 0
+
+  var usages = [];
 
   function criteria(o1, o2) {
     return o1.number < o2.number ? -1
-         : o1.number > o2.number ?  1 
+         : o1.number > o2.number ?  1
          : 0
   }
 
   return through(function(data, enc, done) {
     var res = JSON.parse(data)
     storage.push(res)
-    if (storage.length > maxStorageSize) { 
+    if (storage.length > maxStorageSize) {
       //storage.sort(criteria)
       this.push(JSON.stringify(storage) + '\n')
       storage = []
-      if (++gens % interval === 0) {
+      if (global.gc) global.gc();
+      var memUse = process.memoryUsage().rss / 1024 / 1024;
+      usages.push(memUse);
+      console.log("Repetition", usages.length, "of", repetitions, ":", memUse.toFixed(1), "MB")
+      if (usages.length >= repetitions) {
         console.log("=========================")
-        console.log("Generation", gens)
+	    console.log("Peak memory usage", Math.max.apply(Math, usages).toFixed(1), "MB")
         console.log("=========================")
+        process.exit(0)
       }
     }
     done()
